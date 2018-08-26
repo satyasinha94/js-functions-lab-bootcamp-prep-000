3898ac220b69c5c330c9c9a976e8a4b60f3937b1
satyasinha94 <satyasinha94@gmail.com>
2018-08-26 20:52:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown(holiday, days) {
+  return `it's ${days} days until ${holiday}!`
+}
