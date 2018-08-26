81ff3361c893d4523b196f8d22982412b41853f7
satyasinha94 <satyasinha94@gmail.com>
2018-08-26 20:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  var name = you
-  return 'Happy holidays, ${name}!'
+  return `Happy holidays, ${name}!`
 }
